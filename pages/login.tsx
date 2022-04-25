e2bbf07d19db6db88cbcfcf66caccc1f0988c281
Israel Santiago <israel.santiago4359@gmail.com>
2022-04-24 20:57:07 -0700

    handle logion form errors
    - immer lib added to handle states
    - react-hook-form to handle login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useImmer } from 'use-immer'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 const Login = () => {
+  const [login, setLogin] = useImmer<boolean>(false)
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
@@ -22,14 +39,37 @@ const Login = () => {
         height={150}
       />
 
-      <form className="md:max-x-md relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="md:max-x-md relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:px-14"
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register('password', { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid password.
+              </p>
+            )}
           </label>
         </div>
 
